4f88a57783bf3e039432fa0b83cfcea02d50fbab
SPR-MD6 <spr-md6@asynchrony.com>
2017-07-05 09:34:27 -0500

    000 - empty xcodeproj - CU DH

---
+++ SparkCallkitApp/SparkCallkitApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SparkCallkitApp
+//
+//  Created by Dev1 on 7/5/17.
+//  Copyright © 2017 asynchrony. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
