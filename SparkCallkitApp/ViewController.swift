47784424a6df6767645ad5792ea6bf0bcb89d645
SPR-MD6 <spr-md6@asynchrony.com>
2017-07-05 09:54:15 -0500

    000 - set up base view controller - DH CU

--- SparkCallkitApp/ViewController.swift
+++ SparkCallkitApp/ViewController.swift
@@ -2,6 +2,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let wilmaButton: UIButton = {
+        let button = UIButton()
+        
+        button.setTitle("Wilma", for: .normal)
+        button.backgroundColor = .gray
+        button.setTitleColor(.black, for: .normal)
+        
+        return button
+    }()
+    
+    let xavierButton: UIButton = {
+        let button = UIButton()
+        
+        button.setTitle("Xavier", for: .normal)
+        button.backgroundColor = .gray
+        button.setTitleColor(.black, for: .normal)
+        
+        return button
+    }()
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        
+        view.backgroundColor = .white
+        
+        view.addSubview(wilmaButton)
+        view.addSubview(xavierButton)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -12,6 +45,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        wilmaButton.sizeToFit()
+        wilmaButton.frame.origin.x = view.frame.midX - wilmaButton.frame.width / 2
+        wilmaButton.frame.origin.y = view.frame.midY - wilmaButton.frame.height - 10
+        
+        xavierButton.sizeToFit()
+        xavierButton.frame.origin.x = view.frame.midX - xavierButton.frame.width / 2
+        xavierButton.frame.origin.y = view.frame.midY + xavierButton.frame.height + 10
+    }
 }
 
